59e581219e934a5b030837806e7b75ade82e9d29
choko5901 <ehdgk5901@gmail.com>
2022-08-19 20:54:51 +0900

    test 1th

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+// import HomePage from './components/HomePage';
+// import Users from './components/Users';
+import Jingu from './components/Jingu';
+function App() {
+  return (
+    <>
+    <Jingu />
+    {/* <Users /> */}
+    {/* <HomePage /> */}
+    </>
+  );
+}
+
+export default App;
