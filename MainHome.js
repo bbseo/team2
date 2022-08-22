c3cf7cc39585a677e4dbd65ef5ad82ac2910843c
suk97 <92973771+suk97@users.noreply.github.com>
2022-08-22 11:44:41 +0900

    Add files via upload

---
+++ MainHome.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./Header";
+import Content from "./Content";
+import Footer from "./Footer"
+import styled from "styled-components";
+import Header2 from "./Header2";
+
+
+function MainHome() {
+    return(
+        <div>
+            <Header2 />
+            <Content />
+            <Footer />
+        </div>
+    )
+}
+
+export default MainHome;
